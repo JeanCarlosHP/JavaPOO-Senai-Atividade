772af4437463a7a68a3c6c92e29bf9dca44149a6
JeanCarlosHP <jeanchpaula@gmail.com>
2021-07-01 09:27:09 -0300

    Refatoração nos cases do switch

--- src/br/com/senai/ProgramaPrincipal.java
+++ src/br/com/senai/ProgramaPrincipal.java
@@ -14,48 +14,47 @@ public class ProgramaPrincipal {
 	public static void main(String[] args) {
 		List<Aluno> alunos = new ArrayList<Aluno>();
 		List<Curso> cursos = new ArrayList<Curso>();
-		
+
 		Scanner in = new Scanner(System.in);
 		boolean sair = true;
-		
+
 		AlunoController alunoController = new AlunoController();
 		CursoController cursoController = new CursoController();
-		
+
 		do {
 			menuPrincipal();
 			int opcao = in.nextInt();
-			
-			switch (opcao) {
-			case 1:
-				alunoController.menuAluno(alunos);
-				break;
-				
-			case 2:
-				cursoController.menuCurso(cursos);
-				break;
-				
-			case 0:
-				System.out.println("\nSistema Finalizado!");
-				sair = false;
-				break;
-				
-			default:
-				System.out.println("Opção Inválida!");
-				break;
-			}
-			
-			
-		} while (sair);
 
+			switch (opcao) {
+				case 1:
+					alunoController.menuAluno(alunos, cursos);
+					break;
+
+				case 2:
+					cursoController.menuCurso(cursos, alunos);
+					break;
+
+				case 0:
+					System.out.println("\nSistema Finalizado!");
+					sair = false;
+					break;
+
+				default:
+					System.out.println("Opï¿½ï¿½o Invï¿½lida!");
+					break;
+			}
+
+		} while (sair);
+		in.close();
 	}
-	
+
 	public static void menuPrincipal() {
 		System.out.println("\n=== MENU PRINCIPAL ===");
 		System.out.println("1 - Registro Aluno");
 		System.out.println("2 - Registro Curso");
 		System.out.println("0 - Sair");
 		System.out.println("======================");
-		
+
 		System.out.print("> ");
 	}
 
