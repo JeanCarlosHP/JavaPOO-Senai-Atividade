43ce6ee10e51f2425785696d472d6e9e7620606b
JeanCarlosHP <jeanchpaula@gmail.com>
2021-07-01 09:30:23 -0300

    Add a opção de atribuir um curso ao aluno no método cadastrarAluno

--- src/br/com/senai/aluno/AlunoController.java
+++ src/br/com/senai/aluno/AlunoController.java
@@ -3,120 +3,247 @@ package br.com.senai.aluno;
 import java.util.List;
 import java.util.Scanner;
 
+import br.com.senai.curso.Curso;
+import br.com.senai.curso.CursoController;
+
 public class AlunoController {
-	
+
 	private Scanner in;
-	
+
 	public AlunoController() {
 		in = new Scanner(System.in);
 	}
-	
-	public void menuAluno(List<Aluno> alunos) {
+
+	public void menuAluno(List<Aluno> alunos, List<Curso> cursos) {
 		System.out.println("\n==== MENU ALUNO ====");
 		System.out.println("1 - Cadastrar Aluno");
 		System.out.println("2 - Listar Alunos");
 		System.out.println("3 - Editar Aluno");
 		System.out.println("4 - Excluir Aluno");
 		System.out.println("====================");
-		
+
 		System.out.print("> ");
 		int opcao = in.nextInt();
-		
+
 		switch (opcao) {
-		case 1:
-			alunos.add(cadastrarAluno());
-			break;
-			
-		case 2:
-			listarAlunos(alunos);
-			break;
-			
-		case 3:
-			editarAluno(alunos);
-			break;
-			
-		case 4:
-			excluirAluno(alunos);
-			break;
-			
-		default:
-			System.out.println("Opção Inválida!");
-			break;
+			case 1:
+				alunos.add(cadastrarAluno(cursos));
+				break;
+
+			case 2:
+				listarAlunos(alunos);
+				break;
+
+			case 3:
+				editarAluno(alunos);
+				break;
+
+			case 4:
+				excluirAluno(alunos);
+				break;
+
+			default:
+				System.out.println("Opï¿½ï¿½o Invï¿½lida!");
+				break;
 		}
 	}
-	
-	public Aluno cadastrarAluno() {
-		Aluno alunos = new Aluno();
-		
+
+	public Aluno cadastrarAluno(List<Curso> cursos) {
+
+		if (cursos.isEmpty()) {
+			System.out.println("\nNÃ£o possui cursos cadastrados.");
+			return null;
+		}
+
+		System.out.println("\n=== CADASTRAR ALUNO ===");
+
+		CursoController cursoController = new CursoController();
+		Aluno aluno = new Aluno();
+
+		cursoController.listarCursos(cursos);
+
+		System.out.println("\n--- Dados do curso ---");
+
+		System.out.print("Informe o Id do curso que o aluno deseja participar: ");
+		int idCurso = in.nextInt() - 1;
+
+		aluno.setCurso(cursos.get(idCurso));
+
+		System.out.println("\n--- Dados do aluno ---");
+
 		in.nextLine();
-		
-		System.out.print("\nInforme o nome: ");
-		alunos.setNomeAluno(in.nextLine());
-		
+
+		System.out.print("Informe o nome: ");
+		aluno.setNomeAluno(in.nextLine());
+
 		System.out.print("Informe a idade: ");
-		alunos.setIdadeAluno(in.nextInt());
-		
-		System.out.print("informe o pais: ");
+		aluno.setIdadeAluno(in.nextInt());
+
+		System.out.print("informe o paÃ­s: ");
 		in.nextLine();
-		alunos.setPais(in.nextLine());
-		
+		aluno.setPais(in.nextLine());
+
 		System.out.print("Informe o estado: ");
-		alunos.setEstado(in.nextLine());
-		
+		aluno.setEstado(in.nextLine());
+
 		System.out.print("Informe a cidade: ");
-		alunos.setCidade(in.nextLine());
-		
+		aluno.setCidade(in.nextLine());
+
 		System.out.println("\nAluno Cadastrado!");
-		
-		return alunos;
+
+		return aluno;
 	}
-	
+
 	public List<Aluno> listarAlunos(List<Aluno> alunos) {
-		
+
 		if (alunos.isEmpty()) {
-			System.out.println("\nNão possui alunos cadastrados.");
+			System.out.println("\nNï¿½o possui alunos cadastrados.");
 			return null;
 		}
 
 		System.out.println("\n=== ALUNOS CADASTRADOS ===");
 
-		System.out.printf(
-				"| %2s | %4s | %5s | %6s | %14s | %14s |\n", 
-				"Id", "Nome", "Idade", "Pais", "Estado", "Cidade");
+		System.out.printf("| %2s | %4s | %5s | %6s | %14s | %14s | %10s |\n", "Id", "Nome", "Idade", "PaÃ­s", "Estado",
+				"Cidade", "Nome Curso");
 
 		for (int i = 0; i < alunos.size(); i++) {
-			System.out.printf(
-					"| %2d | %4s | %5d | %6s | %14s | %14s |\n",
-					i + 1, 
-					alunos.get(i).getNomeAluno(), 
-					alunos.get(i).getIdadeAluno(), 
-					alunos.get(i).getPais(), 
-					alunos.get(i).getEstado(), 
-					alunos.get(i).getCidade()
-					);
+			System.out.printf("| %2d | %4s | %5d | %6s | %14s | %14s | %10s |\n", i + 1, alunos.get(i).getNomeAluno(),
+					alunos.get(i).getIdadeAluno(), alunos.get(i).getPais(), alunos.get(i).getEstado(),
+					alunos.get(i).getCidade(), alunos.get(i).getCurso().getNomeCurso());
 		}
-		
+
 		return alunos;
 	}
-	
+
 	public List<Aluno> editarAluno(List<Aluno> alunos) {
-		
+
 		if (alunos.isEmpty()) {
-			System.out.println("\nNão possui alunos cadastrados.");
+			System.out.println("\nNï¿½o possui alunos cadastrados.");
 			return null;
 		}
-		
-		
+
+		listarAlunos(alunos);
+
+		Aluno aluno = new Aluno();
+
+		System.out.println("\n=== EDITAR ALUNO ===");
+
+		System.out.print("Informe o Id do aluno para editar: ");
+		int idAluno = in.nextInt() - 1;
+
+		System.out.println("\n1) Editar o nome do aluno");
+		System.out.println("2) Editar a idade do aluno");
+		System.out.println("3) Editar o paÃ­s do aluno");
+		System.out.println("4) Editar o estado do aluno");
+		System.out.println("5) Editar a cidade do aluno");
+
+		System.out.print("\nInforme o campo para ser editado: ");
+		int campo = in.nextInt();
+		in.nextLine();
+
+		switch (campo) {
+			case 1:
+				System.out.println("\n--- Editar nome ---");
+
+				System.out.print("Informe o novo nome: ");
+				aluno.setNomeAluno(in.nextLine());
+
+				aluno.setIdadeAluno(alunos.get(idAluno).getIdadeAluno());
+				aluno.setPais(alunos.get(idAluno).getPais());
+				aluno.setEstado(alunos.get(idAluno).getEstado());
+				aluno.setCidade(alunos.get(idAluno).getCidade());
+
+				System.out.println("\nNome alterado.");
+				break;
+
+			case 2:
+				System.out.println("\n--- Editar idade ---");
+
+				System.out.print("Informe a nova idade: ");
+				aluno.setIdadeAluno(in.nextInt());
+
+				aluno.setNomeAluno(alunos.get(idAluno).getNomeAluno());
+				aluno.setPais(alunos.get(idAluno).getPais());
+				aluno.setEstado(alunos.get(idAluno).getEstado());
+				aluno.setCidade(alunos.get(idAluno).getCidade());
+
+				System.out.println("\nIdade alterada.");
+				break;
+
+			case 3:
+				System.out.println("\n--- Editar paÃ­s ---");
+
+				System.out.print("Informe o novo paÃ­s: ");
+				aluno.setPais(in.nextLine());
+
+				aluno.setNomeAluno(alunos.get(idAluno).getNomeAluno());
+				aluno.setIdadeAluno(alunos.get(idAluno).getIdadeAluno());
+				aluno.setEstado(alunos.get(idAluno).getEstado());
+				aluno.setCidade(alunos.get(idAluno).getCidade());
+
+				System.out.println("\nPaÃ­s alterado.");
+				break;
+
+			case 4:
+				System.out.println("\n--- Editar estado ---");
+
+				System.out.print("Informe o novo estado: ");
+				aluno.setEstado(in.nextLine());
+
+				aluno.setNomeAluno(alunos.get(idAluno).getNomeAluno());
+				aluno.setIdadeAluno(alunos.get(idAluno).getIdadeAluno());
+				aluno.setPais(alunos.get(idAluno).getPais());
+				aluno.setCidade(alunos.get(idAluno).getCidade());
+
+				System.out.println("\nEstado alterado.");
+				break;
+
+			case 5:
+				System.out.println("\n--- Editar cidade ---");
+
+				System.out.print("Informe a nova cidade: ");
+				aluno.setCidade(in.nextLine());
+
+				aluno.setNomeAluno(alunos.get(idAluno).getNomeAluno());
+				aluno.setIdadeAluno(alunos.get(idAluno).getIdadeAluno());
+				aluno.setPais(alunos.get(idAluno).getPais());
+				aluno.setEstado(alunos.get(idAluno).getEstado());
+
+				System.out.println("\nCidade alterada.");
+				break;
+
+			default:
+				System.out.println("\nOpÃ§Ã£o InvÃ¡lida!");
+				break;
+		}
+
+		alunos.set(idAluno, aluno);
+
 		return alunos;
 	}
-	
+
 	public void excluirAluno(List<Aluno> alunos) {
-		
+
 		if (alunos.isEmpty()) {
-			System.out.println("\nNão possui alunos cadastrados.");
+			System.out.println("\nNï¿½o possui alunos cadastrados.");
+			return;
 		}
-		
-		
-		System.out.println("Aluno excluído!");
+
+		listarAlunos(alunos);
+
+		System.out.println("\n=== EXCLUIR ALUNO ===");
+
+		System.out.print("Informe o Id do aluno para excluir: ");
+		int idAluno = in.nextInt() - 1;
+
+		if (alunos.size() <= idAluno) {
+			System.out.println("\nAluno nï¿½o cadastrado.");
+			return;
+		}
+
+		alunos.remove(idAluno);
+
+		System.out.println("\nAluno excluÃ­do!");
+
 	}
 }
