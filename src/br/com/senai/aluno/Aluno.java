d4c3e944e75c70d109d8eff05b279c996ae550dd
JeanCarlosHP <jeanchpaula@gmail.com>
2021-07-01 09:29:28 -0300

    Add a opção de atribuir um curso ao aluno no método cadastrarAluno

--- src/br/com/senai/aluno/Aluno.java
+++ src/br/com/senai/aluno/Aluno.java
@@ -1,26 +1,34 @@
 package br.com.senai.aluno;
 
-import java.util.List;
-import java.util.ArrayList;
-
 import br.com.senai.curso.Curso;
 import br.com.senai.endereco.Endereco;
 
 public class Aluno extends Endereco {
 	private String nomeAluno;
 	private int idadeAluno;
-	private List<Curso> cursos = new ArrayList<Curso>();
-	
+	private Curso curso = new Curso();
+
 	public String getNomeAluno() {
 		return nomeAluno;
 	}
+
 	public void setNomeAluno(String nomeAluno) {
 		this.nomeAluno = nomeAluno;
 	}
+
 	public int getIdadeAluno() {
 		return idadeAluno;
 	}
+
 	public void setIdadeAluno(int idadeAluno) {
 		this.idadeAluno = idadeAluno;
+	}
+
+	public Curso getCurso() {
+		return curso;
+	}
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
 	}
 }
